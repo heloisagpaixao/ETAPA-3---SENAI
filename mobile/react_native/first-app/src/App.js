39cb35064cbbd82270aec1047b229932ab1c81f6
heloísa <heloisa.paixao@aluno.senai.br>
2026-02-23 10:00:10 -0300

    atualizações (23/02/2026)

--- mobile/react_native/first-app/src/App.js
+++ mobile/react_native/first-app/src/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Exemplo01 from './jsx_examples/exemplo-01-estrutura';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Exemplo01/>
       <StatusBar style="auto" />
     </View>
   );
