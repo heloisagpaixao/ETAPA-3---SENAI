f1bd8b8a5d89d173407c5be080bb62edff77affa
heloísa <heloisa.paixao@aluno.senai.br>
2026-02-26 14:02:04 -0300

    atualizações (26/02/2026)

---
+++ frontEnd/aula3_exemplo-techFood/src/scripts/aula4.js
@@ -0,0 +1,36 @@
+class Prato {
+    constructor(nome, preco) {
+        this.nome = nome,
+            this.preco = preco
+    }
+}
+
+const lasanha = new Prato("Lasana Bolonhesa", 45.00)
+
+// alert("Seja bem-vindo ao restaurante Sabor e Saber!")
+// console.log("Teste")
+
+const cliente = prompt("Bem-vindo, cliente! Para um atendimento personalizado, digite o seu nome:")
+
+let nomeFormatado = cliente.trim().toUpperCase()
+alert(`Muito prazer, ${nomeFormatado}!`)
+
+// const horaAgora = new Date()
+
+// const hora = horaAgora.getHours()
+// if (hora < 11) {
+//     alert(`Bom dia, ${nomeFormatado}! Aproveite as delícias de o café da manhã!`)
+//     console.log(`Antes das 11h00.`) 
+//     } else {
+//         alert(`Boa tarde, ${nomeFormatado}! Aproveite os nossos pratos para almoço e jantar!`)
+//         console.log(`Depois das 11h00.`)
+//     }
+
+const querPrato = confirm(`Olá, ${nomeFormatado}! Quer um prato?`)
+if (querPrato) {
+    let quantidade = prompt(`Hoje temos Lasanha Bolonhesa, quantas você quer?`)
+    let total = lasanha.preco * quantidade
+    alert(total)
+} else {
+    alert(`Ok! Obrigado pela visita e volte sempre!!`)
+}
