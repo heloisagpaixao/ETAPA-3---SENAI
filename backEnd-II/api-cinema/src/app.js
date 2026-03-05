689108eb8126143d8f7096b63d92b6cccb2043fe
heloísa <heloisa.paixao@aluno.senai.br>
2026-03-05 08:46:25 -0300

    ANIVERSARIO (05/03/2026)

--- backEnd-II/api-cinema/src/app.js
+++ backEnd-II/api-cinema/src/app.js
@@ -1,16 +1,42 @@
+// const express = require('express')
+// const pool = require('.config/database')
+
+// const app = express()
+
+// app.use(express.json())
+
+// app.get('/', (req, res) => {
+//     res.send("API CINEMA")
+// })
+
+// app.get('/filmes', (req, res) => {
+//     pool.query('SELECT * FROM filme', (err, results) =>{
+//         res.json(results)
+//     })
+// })
+
 const express = require('express')
-const pool = require('.config/database')
+const pool = require('./config/database')
 
 const app = express()
-
 app.use(express.json())
 
-app.get('/', (req, res) => {
+app.get('/', (req,res) => {
     res.send("API CINEMA")
 })
 
-app.get('/filmes', (req, res) => {
+app.get('/filmes', (req,res) => {
     pool.query('SELECT * FROM filme', (err, results) =>{
         res.json(results)
     })
 })
+
+app.get('/filmes/:id', (req,res) => {
+    const {id} = req.params
+
+    pool.query('SELECT * FROM filme WHERE id = ?', [id],(err, results) =>{
+        res.json(results)
+    })
+})
+
+module.exports = app
