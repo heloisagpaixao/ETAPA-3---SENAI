a5cbdc12d2829dd708f11ae83867f860586fd1aa
heloísa <heloisa.paixao@aluno.senai.br>
2026-03-09 10:50:16 -0300

    atualizações (09/03/2026)

--- internetDasCoisas/revisao/atuador_button.py
+++ internetDasCoisas/revisao/atuador_button.py
@@ -1,15 +1,30 @@
 from machine import Pin
 from utime import sleep
 
-button_pull_down = Pin(15, Pin.IN)
+# button_pull_down = Pin(15, Pin.IN)
+# 
+# while True:
+#     #Variável que armazenará o estado do botão
+#     estado_botao = button_pull_down.value()
+#     
+#     if estado_botao == 1:
+#         print("1 - Botão pressionado! (HIGH)")
+#     else:
+#         print("0 - Botão não pressionado! (LOW)")
+# 
+#     sleep(0.2)
+    
+###--------------------------------------------------###
+
+button_pull_up = Pin(15, Pin.IN)
 
 while True:
     #Variável que armazenará o estado do botão
-    estado_botao = button_pull_down.value()
+    estado_botao = button_pull_up.value()
     
-    if estado_botao == 1:
-        print("1 - Botão pressionado! (HIGH)")
+    if estado_botao == 0:
+        print("0 - Botão pressionado! (LOW)")
     else:
-        print("0 - Botão não pressionado! (LOW)")
+        print("1 - Botão não pressionado! (HIGH)")
 
     sleep(0.2)
