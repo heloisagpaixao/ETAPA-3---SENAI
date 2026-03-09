231bf2753d5c083be1b3369f39881ebf70c3ce2c
heloísa <heloisa.paixao@aluno.senai.br>
2026-03-09 14:06:46 -0300

    atualizações (09/03/2026)

--- mobile/react_native/first-app/src/basic_components/view_examples01.js
+++ mobile/react_native/first-app/src/basic_components/view_examples01.js
@@ -1,51 +1,57 @@
 import { View, Text, StyleSheet } from "react-native";
 
 export default function ViewExamples01() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.titulo}>Flex Direction</Text>
+    return (
+        <View style={styles.container}>
+            <Text style={styles.titulo}>Flex Direction</Text>
 
-      <View style={styles.exemplo}></View>
-    </View>
-  );
+            <View style={styles.exemplo}>
+                <View style={{ flexDirection: "row", gap: 8 }}>
+                    <View style={styles.box} />
+                    <View style={styles.box} />
+                    <View style={styles.box} />
+                </View>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#f5f5f5",
-  },
-  titulo: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginBottom: 20,
-  },
-  subtitulo: {
-    fontSize: 14,
-    fontWeight: "bold",
-    color: "#4285f4",
-    marginBottom: 8,
-  },
-  exemplo: {
-    width: "80%",
-    padding: 16,
-    marginBottom: 16,
-    backgroundColor: "#fff",
-    borderRadius: 8,
-  },
-  box: {
-    width: 50,
-    height: 50,
-    backgroundColor: "#4285f4",
-    borderRadius: 4,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  textBox: {
-    color: "#fff",
-    fontSize: 12,
-    fontWeight: "bold",
-  },
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#f5f5f5",
+    },
+    titulo: {
+        fontSize: 20,
+        fontWeight: "bold",
+        marginBottom: 20,
+    },
+    subtitulo: {
+        fontSize: 14,
+        fontWeight: "bold",
+        color: "#4285f4",
+        marginBottom: 8,
+    },
+    exemplo: {
+        width: "80%",
+        padding: 16,
+        marginBottom: 16,
+        backgroundColor: "#fff",
+        borderRadius: 8,
+    },
+    box: {
+        width: 50,
+        height: 50,
+        backgroundColor: "#4285f4",
+        borderRadius: 4,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    textBox: {
+        color: "#fff",
+        fontSize: 12,
+        fontWeight: "bold",
+    },
 });
