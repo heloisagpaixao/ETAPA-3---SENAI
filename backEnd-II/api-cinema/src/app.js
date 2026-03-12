813622cb763e68074b78a1a11a7f3a03987572b0
heloísa <heloisa.paixao@aluno.senai.br>
2026-03-12 09:45:09 -0300

    atualizações (12/03/2026)

--- backEnd-II/api-cinema/src/app.js
+++ backEnd-II/api-cinema/src/app.js
@@ -39,39 +39,117 @@ app.get('/filmes', async(req,res) => {
     // })
 })
 
-app.get('/filmes/:id', async(req,res) => {
-    try{
-        const {id} = req.params
-        if(!id ||  isNaN(id)){
+// app.get('/filmes/:id', async(req,res) => {
+//     try{
+//         const {id} = req.params
+//         if(!id ||  isNaN(id)){
+//             return res.status(400).json({
+//                 sucesso: false,
+//                 mensagem: `ID de filme inválido!`
+//             })
+//         }
+//         const filme = await query.async(`SELECT * FROM filme WHERE id = ?`,[id])
+//         if (filme.length === 0){
+//             return res.status (404).json({
+//                 sucesso: false,
+//                 mensagem: `Filme não encontrado.`
+//             })
+//         }
+//         res.json({
+//             sucesso: true,
+//             dados: filme[0]
+//         })
+//     } catch (erro) {
+//         console.error(`Erro ao encontrar filme: ${erro}.`)
+//         res.status(500).json({
+//             sucesso: false,
+//             mensagem: `Erro ao encontrar filme.`,
+//             erro: erro.message
+//         })
+//     }
+//     // const {id} = req.params
+
+//     // pool.query('SELECT * FROM filme WHERE id = ?', [id],(err, results) =>{
+//     //     res.json(results)
+//     // })
+// })
+
+app.get('/filmes/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+
+        if (!id || isNaN(id)) {
             return res.status(400).json({
                 sucesso: false,
-                mensagem: `ID de filme imválido!`
+                mensagem: 'ID de filme inválido.'
             })
         }
-        const filme = await query.async(`SELECT * FROM filme WHERE id = ?`,[id])
-        if (filme.length === 0){
-            return res.sttaus (404).json({
+
+        const filme = await queryAsync('SELECT * FROM filme WHERE id = ?', [id])
+
+        if (filme.length === 0) {
+            return res.status(404).json({
                 sucesso: false,
-                mensagem: `Filme não encontrado.`
+                mensagem: 'Filme não encontrado'
             })
         }
+
         res.json({
             sucesso: true,
             dados: filme[0]
         })
+
     } catch (erro) {
-        console.error(`Erro ao encontrar filme: ${erro}.`)
+        console.error('Erro ao encontrar filme:', erro)
         res.status(500).json({
             sucesso: false,
-            mensagem: `Erro ao encontrar filme.`,
+            mensagem: 'Erro ao encontrar filme',
             erro: erro.message
         })
     }
-    // const {id} = req.params
-
-    // pool.query('SELECT * FROM filme WHERE id = ?', [id],(err, results) =>{
-    //     res.json(results)
-    // })
 })
+
+app.post('/filmes', async(req,res) =>{
+    try {
+        const {titulo, genero, duracao, classificacao, data_lancamento} = req.body
+
+        if(!titulo || !genero || !duracao){
+            return res.status(400).json({
+                sucesso: false,
+                mensagem: 'Título, genero e ducação são obrigatórios'
+            })
+        }
+
+        if(typeof duracao !== 'number' || duracao <= 0 ){
+            return res.status(400).json({
+                sucesso: false,
+                mensagem: 'Duração deve ser um número positivo.'
+            })
+        }
+
+        const novoFilme = {
+            titulo: titulo.trim(),
+            genero: genero.trim(),
+            duracao,
+            classificacao: classificacao || null,
+            data_lancamento: data_lancamento || null
+        }
+
+        const resultado = await queryAsync('INSERT INTO filme SET ?',[novoFilme])
+
+        res.status(201).json({
+            sucesso: true,
+            mensagem: 'Filme cadastrado com sucesso.',
+            id: resultado.insertId
+        })
+    } catch (erro) {
+        console.error('Erro ao salvar filme:', erro)
+        res.status(500).json({
+            sucesso: false,
+            mensagem: 'Erro ao salvar filme.',
+            erro: erro.message
+        })
+    }
+} )
 
 module.exports = app
