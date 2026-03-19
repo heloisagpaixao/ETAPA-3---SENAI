35c886b683e6b62e546c17e7bb6cfb034d511063
heloísa <heloisa.paixao@aluno.senai.br>
2026-03-19 14:32:31 -0300

    atualizações (19/03/2026)

--- frontEnd/aula5_DOM/exemplos/src/scripts/aula6.js
+++ frontEnd/aula5_DOM/exemplos/src/scripts/aula6.js
@@ -7,9 +7,21 @@ if(inputQtd && precoTexto){
         const precoUnitario = 45.0
         const total = Number((inputQtd.value) * precoUnitario)
         precoTexto.textContent = `R$ ${total.toFixed(2)}`
-        
-        precoTexto.stylecolor= total >= 150 ? "#c0392b": "#e67e22"
+        precoTexto.stylecolor = total > 150 ? "#c0392b" : "#e67e22"
     })
 }
 
 // 2. EVENTOS DE CLIQUES PARA CLASS - EVENT.TARGET
+document.addEventListener('click', (event) => {
+    const clicado = event.target
+    // fazer a ação/mudança/aplicação que você quiser, mediante ao que foi clicado.
+})
+
+const massas = document.querySelector('#secao-massas')
+massas.addEventListener('click', (event) =>{
+    const clicado = event.target
+    
+    if(clicado.classList.contains('btn-pedido')){
+        console.log('Você clicou em um botão de MASSAS!!')
+    }
+})
