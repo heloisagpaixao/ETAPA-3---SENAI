fdfeb44b15ad2b603649fb726ba1334b5939be4e
heloísa <heloisa.paixao@aluno.senai.br>
2026-03-19 13:26:36 -0300

    atualizações (19/03/2026)

--- frontEnd/aula5_DOM/exemplos/src/scripts/aula6.js
+++ frontEnd/aula5_DOM/exemplos/src/scripts/aula6.js
@@ -1,9 +1,15 @@
 const inputQtd = document.querySelector('#qtd-lasanha')
 const precoTexto = document.querySelector('#preco-lasanha')
 
+// CÁLCULO DE PREÇO
 if(inputQtd && precoTexto){
     inputQtd.addEventListener("input", () => {
         const precoUnitario = 45.0
-        const total = Number((inputQtd.value) * precoUnitario).toFixed(2)
+        const total = Number((inputQtd.value) * precoUnitario)
+        precoTexto.textContent = `R$ ${total.toFixed(2)}`
+        
+        precoTexto.stylecolor= total >= 150 ? "#c0392b": "#e67e22"
     })
 }
+
+// 2. EVENTOS DE CLIQUES PARA CLASS - EVENT.TARGET
