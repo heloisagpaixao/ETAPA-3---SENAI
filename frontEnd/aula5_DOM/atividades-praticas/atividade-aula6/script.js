e56646d0fcdb689636305bb6aa5e45525e13dad2
heloísa <heloisa.paixao@aluno.senai.br>
2026-03-22 10:38:43 -0300

    atualizações (22/03/2026)

--- frontEnd/aula5_DOM/atividades-praticas/atividade-aula6/script.js
+++ frontEnd/aula5_DOM/atividades-praticas/atividade-aula6/script.js
@@ -1,5 +1,43 @@
-let contadorDeCurtidas = document.querySelector('#contador')
+// SELEÇÃO DE ELEMENTOS DO DOM
+let contador = document.querySelector("#contador");
+let btnCurtir = document.querySelector("#btn-curtir");
+const campoTexto = document.querySelector("#campo-texto");
+const previewTexto = document.querySelector("#preview-texto");
+const caixaCor = document.querySelector("#caixa-cor");
+let btnReset = document.querySelector("#btn-reset")
 
-contadorDeCurtidas.addEventListener('click', (event) =>{
-    
+
+// 1. INTERAÇÃO (CLIQUE)
+let totalCurtidas = 0
+
+btnCurtir.addEventListener("click", () => {
+    totalCurtidas = totalCurtidas + 1 // altera o valor da variável
+    contador.textContent = totalCurtidas // atualiza o valor na tela
+})
+
+
+// 2. MONITORAMENTO (INPUT)
+campoTexto.addEventListener("input", () => {
+    const textoDigitado = campoTexto.value //valor atual digitado
+    previewTexto.textContent = "Digitando: " + textoDigitado // atualiza o texto do preview
+})
+
+
+// 3. SENSORES (MOUSE)
+caixaCor.addEventListener("mouseenter", () => { // quando o mouse entrar na caixa
+    caixaCor.style.backgroundColor = "blue" //altera a cor de fundo
+})
+
+
+caixaCor.addEventListener("mouseleave", () => { // quando o mouse sai da caixa
+    caixaCor.style.backgroundColor = "" // remove a cor
+})
+
+
+// 4. BOTÃO DE RESET
+btnReset.addEventListener("click", () => {
+    totalCurtidas = 0 // zera o número de curtidas
+    contador.textContent = totalCurtidas // atualiza o contador na tela
+    campoTexto.value = " " // limpa o campo de texto
+    previewTexto.textContent = " " // reseta o texto do preview
 })
