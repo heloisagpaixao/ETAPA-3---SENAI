aaccb26bdfcf17938dad3fc030579e240e075727
heloísa <heloisa.paixao@aluno.senai.br>
2026-04-02 15:28:15 -0300

    somativa 1 - frontend
    (02/04/2026)

---
+++ frontEnd/somativa-frontend-v4-heloisa-paixao/readme.md
@@ -0,0 +1,20 @@
+# 💻 Simulador do CinePipoca
+
+    Este é um projeto desenvolvido para a avaliação de Front-End, focado em criar interatividade para um site de um cinema. Nessa aplicação, será possível visualizar: mudança de textos, alteração de estilos, contas matemáticas para cálculo de valor, inserção e apagamento de textos e muito mais!
+
+## 🛠️ Tecnologias Utilizadas
+    1. HTML5 (Semântica e Acessibilidade)
+    2. CSS3 (Estilização e Feedback Visual)
+    3. JavaScript (Manipulação de DOM e Eventos)
+
+## 📁 Estrutura do Projeto
+    O projeto foi organizado seguindo boas práticas de mercado: / ├── index.html └── src/ ├── styles/ (Estilos da página) ├── scripts/ (Lógica de interação) └── images/ (Ativos visuais)
+
+## 🚀 Funcionalidades Implementadas
+    1. Interatividade Visual: Elementos que reagem ao passar do mouse (Hover e manipulação de Classes).
+    2. Saudação Dinâmica: Mensagem de boas-vindas que muda conforme o horário local do usuário.
+    3. Cálculo em Tempo Real: Formulário que processa e exibe resultados assim que o usuário digita.
+    4. Injeção de Conteúdo: Adição de novos itens na tela utilizando manipulação de DOM.
+
+## 👨‍💻 Autora
+    Heloísa Gabrielly Paixão
---
+++ frontEnd/somativa-frontend-v4-heloisa-paixao/src/scripts/script.js
@@ -0,0 +1,61 @@
+// MISSÃO 1 - Recepção do Espectador (Manipulação Estática e Date)
+let saudacao = document.querySelector("#saudacao-cinema");
+
+const tempoAgora = new Date();
+const hora = tempoAgora.getHours();
+
+if (hora >= 0 && hora < 12) {
+    saudacao.textContent = "Bom dia! Bem-vindo à Sessão Matinê!"
+}
+if (hora >= 12 && hora <= 18) {
+    saudacao.textContent = "Boa tarde! Sessão da Tarde liberada!"
+}
+else {
+    saudacao.textContent = "Boa noite! Prepare-se para a Sessão Coruja!"
+};
+
+
+// Missão 2: Sala VIP (Eventos de Mouse e ClassList)
+const bannervip = document.querySelector("#banner-vip");
+
+bannervip.addEventListener('mouseenter', () => {
+    bannervip.classList.add("modo-vip");
+});
+
+bannervip.addEventListener('mouseleave', () => {
+    bannervip.classList.remove("modo-vip");
+});
+
+
+// Missão 3: Bilheteria Express (Evento Input e Value)
+let qtdIngressos = document.querySelector("#qtd-ingressos");
+let totalPagar = document.querySelector("#total-pagar");
+
+qtdIngressos.addEventListener('input', () => {
+    const valor = Number(qtdIngressos.value) * 35.00;
+
+    totalPagar.textContent = `${valor.toFixed(2)}`;
+});
+
+
+// Missão 4: Mural de Críticas (Click, Value, Concatenação e innerHTML) ***
+const btnPublicar = document.querySelector("#btn-publicar");
+const campoFilme = document.querySelector("#nome-filme");
+const muralCriticas = document.querySelector("#mural-criticas");
+
+campoFilme.addEventListener('input', () => {
+    const filmeDigitado = campoFilme.value;
+    const filmeFormatado = filmeDigitado.concat()
+
+    btnPublicar.addEventListener("click", () => {
+        muralCriticas.innerHTML += '<article class="card-critica"><h3>🎬 Filme: ' + filmeFormatado + '⭐</h3></article>';
+    })
+});
+
+// Missão 5: Apagar o Quadro (Esvaziar com innerHTML e Focus) ***
+const btnApagar = document.querySelector("#btn-apagar");
+
+btnApagar.addEventListener("click", () => {
+    muralCriticas.value = " "
+    campoFilme.focus()
+})
