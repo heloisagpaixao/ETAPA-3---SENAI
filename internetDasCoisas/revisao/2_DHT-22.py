30b2a6e4000972f7753c87b0730cbd888850dfee
heloísa <heloisa.paixao@aluno.senai.br>
2026-04-10 17:47:30 -0300

    exercício DHT-22 - IoT
    (10/04/2026)

--- internetDasCoisas/revisao/2_DHT-22.py
+++ internetDasCoisas/revisao/2_DHT-22.py
@@ -1,27 +1,26 @@
 from machine import Pin, PWM
 from utime import sleep
-from dht import DHT22 # Biblioteca DHT eu vou importar o DHT22!!!
+from dht import DHT22
 
 sensor_temp = DHT22(Pin(16))
 led = PWM(Pin(14))
 
-led_pwm.freq(1000)
-
+led.freq(1000)  # Corrigido aqui
 
 while True:
-    sensor_temp.measure() # Diz ao sensor que ele iniciará/solicitará uma LEITURA
-    temperatura = sensor_temp.temperature() # Leitura da TEMPERATURA
+    sensor_temp.measure()
+    temperatura = sensor_temp.temperature()
     
     if temperatura < 20:
-        led.duty_u16(65535 * 10 / 100)
-        print(f'ATENÇÃO, {temperatura} °C.')
+        led.duty_u16(int(65535 * 0.1))  # 10%
+        print(f'FRIO: {temperatura} °C - brilho fraco')
     
-    elif temperatura > 19 and temperatura < 31:
-        led.duty_u16(65535 * 50 / 100)
-        print(f'REGULAR, {temperatura} °C.')
+    elif 20 <= temperatura <= 30:
+        led.duty_u16(int(65535 * 0.5))  # 50%
+        print(f'NORMAL: {temperatura} °C - brilho médio')
         
     else:
-        led.duty_u16(65535)
-        print(f'ALERTURA, {temperatura} °C.')
+        led.duty_u16(65535)  # 100%
+        print(f'ALERTA: {temperatura} °C - brilho máximo')
     
-    sleep(2) # Valor recomendado para uma leitura com MAIOR PRECISÃO (não colocar MENOS!!)
+    sleep(2)
